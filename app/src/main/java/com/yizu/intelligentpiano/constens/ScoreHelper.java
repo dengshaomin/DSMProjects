a441b80a8a06028a15d7f3828c2e2cb8b6ff65d0
liusmallpig <chenhuixueba@163.com>
2017-11-02 21:37:08 +0800

    更新同步

--- app/src/main/java/com/yizu/intelligentpiano/constens/ScoreHelper.java
+++ app/src/main/java/com/yizu/intelligentpiano/constens/ScoreHelper.java
@@ -62,12 +62,12 @@ public class ScoreHelper {
     public void setCorrectKey(RectF rectF, SaveTimeData saveTimeData, int bottom) {
         if (correctKeys == null) correctKeys = new ArrayList<>();
         if (rectF.bottom >= bottom && rectF.top <= bottom) {
-            if (saveTimeData.getArriveBottomState() == 0) {
-                saveTimeData.setArriveBottomState(1);
-                correctKeys.add(saveTimeData);
-            } else if (saveTimeData.getArriveBottomState() == 1) {
-                saveTimeData.setArriveBottomState(2);
-            }
+//            if (saveTimeData.getArriveBottomState() == 0) {
+//                saveTimeData.setArriveBottomState(1);
+//                correctKeys.add(saveTimeData);
+//            } else if (saveTimeData.getArriveBottomState() == 1) {
+//                saveTimeData.setArriveBottomState(2);
+//            }
             caRealTimeScores();
             int score = caRealTimeScores();
             if (score != realScore) {
@@ -84,7 +84,7 @@ public class ScoreHelper {
         } else if (rectF.top > bottom && correctKeys.contains(saveTimeData)) {
             hasGoneNodes++;
             Log.e("code", hasGoneNodes + "");
-            saveTimeData.setArriveBottomState(3);
+//            saveTimeData.setArriveBottomState(3);
             correctKeys.remove(saveTimeData);
         }
     }
